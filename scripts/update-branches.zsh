910156d3e18e53344ca2fbe0ffc471a01462fdc0
gladius-mtl <matthew.burton@sap.com>
2020-11-24 13:59:27 -0500

    Make updates to script

--- scripts/update-branches.zsh
+++ scripts/update-branches.zsh
@@ -1,4 +1,15 @@
 #!/bin/zsh
 # Checks out various branches and merges updates from main
 
-git checkout main
+git pull
+
+for branch in v3-develop develop tua-develop Telco-Preprod-Doc
+  do
+
+checkout $branch
+git pull
+git merge main
+:wq
+git push
+
+done
